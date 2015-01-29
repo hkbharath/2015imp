112c5407b2e6b569a3076344913a574292eaa630
hkbharath <hegdebhrathkumar@live.com>
2015-01-29 19:24:27 +0530

    updated frontend validation

--- WebContent/js/start/start.ctrl.js
+++ WebContent/js/start/start.ctrl.js
@@ -1,8 +1,35 @@
 (function(){
 	angular.module('impetus.start',[])
-		.controller('start',function($scope){
+		.controller('start',function($scope,$interval){
 			$scope.showLogin = false;
 			$scope.showSignup = false;
 			$scope.showGuest = false;
+			$scope.time=new Date(); /*get this from server */
+			var getTime = function(){
+				//console.log($scope.time);
+				$scope.time.setTime($scope.time.getTime()+1000);
+			}
+			$interval(getTime,1000);
+			
+			var reset = function(login){
+				if(login){
+					login.$setPristine();
+					login.$setUntouched();
+				}
+			}
+			$scope.loginReset = function(login){
+				reset(login);
+			}
+			
+			$scope.SignupReset = function(signup){
+				reset(signup);
+			}
+			
+			$scope.SignupReset();
+			$scope.loginReset();
+			
+			$scope.signup = function(){
+				
+			}
 		})
 })();
